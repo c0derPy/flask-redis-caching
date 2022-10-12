9d0406c37fa1ffb73f213a68d9c25ce98f059bec
coder <kard3ti@gmail.com>
2022-10-11 21:32:56 -0500

    Add logging

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import request, jsonify
 from config import BaseConfig
 from flask_caching import Cache
 
+import logging
+
+logging.basicConfig(filename='debug.log', 
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 cache = Cache(app)
@@ -10,9 +15,8 @@ cache = Cache(app)
 @app.route('/square/<int:number>')
 @cache.cached(timeout=10)
 def square(number):
-    print("Computing the square of {}".format(number))
-    app.logger.info(f"Computing the square of {number}")
-    return jsonify({"computed":number*number})
+    app.logger.info("Computing the square of {}".format(number))
+    return jsonify({"Computed: ":number*number})
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=8000)
+    app.run()
